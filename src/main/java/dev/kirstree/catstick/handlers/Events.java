7dac89f8b8e25d5494d3717de33c52b5d6483099
Kirstree <kirstree@admin.me>
2022-08-08 10:01:07 +0100

    Added Welcome Message

--- src/main/java/dev/kirstree/catstick/handlers/Events.java
+++ src/main/java/dev/kirstree/catstick/handlers/Events.java
@@ -1,30 +1,28 @@
 package dev.kirstree.catstick.handlers;
 
 
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.minecraft.world.item.ItemCarrotStick;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
+import org.bukkit.*;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
+
 
 public class Events implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = (Player) event.getPlayer();
+
+        if(player.getName().equals("Kirstree")){
+            player.sendMessage(ChatColor.GREEN  + "Hi Boss");
+        }else{
+            player.sendMessage(ChatColor.RED + "How did you get here " + player.getName() + "?");
+        }
 
         ItemStack Catstick = new ItemStack(Material.STICK, 1);
         Inventory inv = player.getInventory();
@@ -36,13 +34,17 @@ public class Events implements Listener {
         inv.setItem(4, Catstick);
     }
 
-    public void onPlayerInteraction(PlayerInteractEvent event){
+    public void onPlayerInteraction(PlayerInteractEvent event) {
         Player player = (Player) event.getPlayer();
 
         if (player.getInventory().getItemInMainHand().getType() == Material.STICK &&
                 event.getAction().equals(Action.RIGHT_CLICK_AIR) ||
                 player.getInventory().getItemInMainHand().getType() == Material.STICK &&
                         event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+
+            /*Cat blackCat = new Cat;
+            blackCat.setCatType(Cat.Type.ALL_BLACK);*/
+
 
         }
     }
