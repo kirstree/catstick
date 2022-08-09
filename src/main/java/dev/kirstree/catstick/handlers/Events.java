d9729295dc123a3a6a1625913cbe1b5519334dca
Kirstree <kirstree@admin.me>
2022-08-09 10:36:45 +0100

    + finished Double Jump Program + if statement for rotating cats - bukkit logger - unused imports

--- src/main/java/dev/kirstree/catstick/handlers/Events.java
+++ src/main/java/dev/kirstree/catstick/handlers/Events.java
@@ -1,8 +1,7 @@
 package dev.kirstree.catstick.handlers;
 
-
-import net.minecraft.world.level.block.Block;
 import org.bukkit.*;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,18 +14,15 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.util.Vector;
 
-import java.awt.event.KeyEvent;
-import java.nio.file.ClosedDirectoryStreamException;
-
 
 public class Events implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = (Player) event.getPlayer();
+        Player player = event.getPlayer();
 
-        if(player.getName().equals("Kirstree")){
-            player.sendMessage(ChatColor.GREEN  + "Hi Boss");
-        }else{
+        if (player.getName().equals("Kirstree")) {
+            player.sendMessage(ChatColor.GREEN + "Hi Boss");
+        } else {
             player.sendMessage(ChatColor.RED + "How did you get here " + player.getName() + "?");
         }
 
@@ -35,16 +31,21 @@ public class Events implements Listener {
 
         ItemMeta meta = Catstick.getItemMeta();
         meta.setDisplayName(ChatColor.RED + ChatColor.BOLD.toString() + "Catstick");
+        meta.addEnchant(Enchantment.LUCK, 2, false);
+        meta.isUnbreakable();
+
         Catstick.setItemMeta(meta);
 
         inv.setItem(4, Catstick);
+
+        if(player.getGameMode() == GameMode.SURVIVAL || player.getGameMode() == GameMode.ADVENTURE){
+            player.setAllowFlight(true);
+        }
     }
 
     @EventHandler
     public void onPlayerInteraction(PlayerInteractEvent event) {
-        Player player = (Player) event.getPlayer();
-
-        //Bukkit.getLogger().info("Some code is working :P");
+        Player player = event.getPlayer();
 
         if (player.getInventory().getItemInMainHand().getType() == Material.STICK &&
                 event.getAction().equals(Action.RIGHT_CLICK_AIR) ||
@@ -59,19 +60,28 @@ public class Events implements Listener {
             blackcat.setVelocity(new Vector(3, 0, 3));
 
         }
+
+        if (player.getInventory().getItemInMainHand().getType() == Material.STICK &&
+                event.getAction().equals(Action.LEFT_CLICK_AIR) || player.getInventory().getItemInMainHand().getType()
+                        == Material.STICK && event.getAction().equals(Action.LEFT_CLICK_BLOCK)){
+
+
+        }
     }
 
-//    @EventHandler
-//    public void onPlayerDoubleJump(PlayerToggleFlightEvent event){
-//        Player player = (Player) event.getPlayer();
-//        GameMode gameMode = player.getGameMode();
-//
-//        if(player.getGameMode() == GameMode.SPECTATOR && player.isFlying() ||
-//                player.getGameMode() == GameMode.SURVIVAL && player.isFlying()){
-//            Block b = player.getWorld().getBlockAt(player.getLocation();
-//
-//        }else{
-//            event.isCancelled();
-//        }
+    @EventHandler
+    public void onPlayerDoubleJump(PlayerToggleFlightEvent event) {
+        Player player = event.getPlayer();
 
+        if(player.getGameMode() == GameMode.SURVIVAL && event.isFlying()
+                || player.getGameMode() == GameMode.ADVENTURE && event.isFlying()){
+
+            if(player.getLocation().subtract(0 , 2, 0).getBlock().getType() != Material.AIR){
+
+                player.setVelocity(player.getLocation().getDirection().multiply(1).setY(1));
+            }
+
+            event.setCancelled(true);
+        }
     }
+}
