33d95ca5cbd6e569c1704d87873bb2fa468539d3
outcastjackalyn <kaos@thegame.com>
2022-08-15 15:15:33 +0100

    p.OpenInventory is here and other things are back where they were

--- src/main/java/dev/kirstree/catstick/handlers/Events.java
+++ src/main/java/dev/kirstree/catstick/handlers/Events.java
@@ -19,87 +19,6 @@ import org.bukkit.util.Vector;
 public class Events implements Listener {
 
 
-    private static Inventory catGUI;
-
-
-    public static Inventory getCatGUI(){
-        return catGUI;
-    }
-    private void gui(Player p){ //moved this all into this class to see if we can run as private function obviously this is less than ideal though
-
-        catGUI = Bukkit.createInventory(null, 54, ChatColor.DARK_PURPLE + "Pick a cat!");
-
-        ItemStack blackCat = new ItemStack(Material.PURPLE_CONCRETE, 1);
-        ItemMeta blackCatMeta = blackCat.getItemMeta();
-        assert blackCatMeta != null;
-        blackCatMeta.setDisplayName(ChatColor.DARK_PURPLE + "Eddie");
-        blackCat.setItemMeta(blackCatMeta);
-
-        ItemStack calicoCat = new ItemStack(Material.CYAN_CONCRETE, 1);
-        ItemMeta calicoCatMeta = calicoCat.getItemMeta();
-        assert calicoCatMeta != null;
-        calicoCatMeta.setDisplayName(ChatColor.DARK_AQUA + "Edward");
-        calicoCat.setItemMeta(calicoCatMeta);
-
-        ItemStack tabbyCat = new ItemStack(Material.LIGHT_BLUE_CONCRETE, 1);
-        ItemMeta tabbyCatMeta = tabbyCat.getItemMeta();
-        assert tabbyCatMeta != null;
-        tabbyCatMeta.setDisplayName(ChatColor.AQUA + "Edmundo");
-        tabbyCat.setItemMeta(tabbyCatMeta);
-
-        ItemStack jellieCat = new ItemStack(Material.MAGENTA_CONCRETE, 1);
-        ItemMeta jellieCatMeta = jellieCat.getItemMeta();
-        assert jellieCatMeta != null;
-        jellieCatMeta.setDisplayName(ChatColor.LIGHT_PURPLE + "Eduardo");
-        jellieCat.setItemMeta(jellieCatMeta);
-
-        ItemStack siameseCat = new ItemStack(Material.LIME_CONCRETE, 1);
-        ItemMeta siameseCatMeta = siameseCat.getItemMeta();
-        assert siameseCatMeta != null;
-        siameseCatMeta.setDisplayName(ChatColor.GREEN + "Edwin");
-        siameseCat.setItemMeta(siameseCatMeta);
-
-        ItemStack whiteCat = new ItemStack(Material.YELLOW_CONCRETE, 1);
-        ItemMeta whiteCatMeta = whiteCat.getItemMeta();
-        assert whiteCatMeta != null;
-        whiteCatMeta.setDisplayName(ChatColor.YELLOW + "Edmund");
-        whiteCat.setItemMeta(whiteCatMeta);
-
-        ItemStack persianCat = new ItemStack(Material.GREEN_CONCRETE, 1);
-        ItemMeta persianCatMeta = persianCat.getItemMeta();
-        assert persianCatMeta != null;
-        persianCatMeta.setDisplayName(ChatColor.DARK_GREEN + "Edgar");
-        persianCat.setItemMeta(persianCatMeta);
-
-        ItemStack oTabbyCat = new ItemStack(Material.BLUE_CONCRETE, 1);
-        ItemMeta oTabbyCatMeta = oTabbyCat.getItemMeta();
-        assert oTabbyCatMeta != null;
-        oTabbyCatMeta.setDisplayName(ChatColor.DARK_BLUE + "Eddison");
-        oTabbyCat.setItemMeta(oTabbyCatMeta);
-
-        ItemStack ragdollCat = new ItemStack(Material.ORANGE_CONCRETE, 1);
-        ItemMeta ragdollCatMeta = ragdollCat.getItemMeta();
-        assert ragdollCatMeta != null;
-        ragdollCatMeta.setDisplayName(ChatColor.RED + "Edith");
-        ragdollCat.setItemMeta(ragdollCatMeta);
-
-        ItemStack bShortCat = new ItemStack(Material.BROWN_CONCRETE, 1);
-        ItemMeta bShortCatMeta = bShortCat.getItemMeta();
-        assert bShortCatMeta != null;
-        bShortCatMeta.setDisplayName(ChatColor.GOLD + "Edwina");
-        bShortCat.setItemMeta(bShortCatMeta);
-
-        catGUI.setItem(21, bShortCat);
-        catGUI.setItem(22, jellieCat);
-        catGUI.setItem(23, blackCat);
-        catGUI.setItem(24, ragdollCat);
-        catGUI.setItem(25, tabbyCat);
-        catGUI.setItem(30, calicoCat);
-        catGUI.setItem(31, oTabbyCat);
-        catGUI.setItem(32, persianCat);
-        catGUI.setItem(33, whiteCat);
-        catGUI.setItem(34, siameseCat);
-    }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
@@ -150,7 +69,7 @@ public class Events implements Listener {
 
             Bukkit.getLogger().info("Right Click is being clicked");
 
-            gui(player);
+            CatGUI.gui(player);
 
             Bukkit.getLogger().info("Catstick is being given");
 
@@ -160,8 +79,7 @@ public class Events implements Listener {
     @EventHandler
     public void inventoryClick(InventoryClickEvent e){
 
-        //if(e.getInventory() == (CatGUI.getCatGUI())){
-        if(e.getInventory() == (catGUI)){
+        if(e.getInventory() == (CatGUI.getCatGUI())){
             Player p = (Player) e.getWhoClicked();
 
             switch (e.getSlot()){
